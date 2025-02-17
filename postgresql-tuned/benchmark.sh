370db55094ff6236324d419d15b2d460aa512d03
Jelte Fennema-Nio <github-tech@jeltef.nl>
2025-02-17 14:19:18 +0100

    Update Postgres version to PG17

--- postgresql-tuned/benchmark.sh
+++ postgresql-tuned/benchmark.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+PGVERSION=17
+
 sudo apt-get update
-sudo apt-get install -y postgresql-common postgresql-14
+sudo apt install -y postgresql-common
+sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y postgresql-common postgresql-$PGVERSION
 
 sudo sed -i -e '
     s/shared_buffers = 128MB/shared_buffers = 8GB/;
     s/#max_parallel_workers = 8/max_parallel_workers=16/;
     s/#max_parallel_workers_per_gather = 2/max_parallel_workers_per_gather = 8/;
     s/max_wal_size = 1GB/max_wal_size=32GB/;
-' /etc/postgresql/14/main/postgresql.conf
+' /etc/postgresql/$PGVERSION/main/postgresql.conf
 
-sudo systemctl restart postgresql@14-main
-
+sudo systemctl restart postgresql@$PGVERSION-main
 
 sudo mkdir /benchmark
 sudo chmod 777 /benchmark
@@ -22,11 +28,11 @@ gzip -d hits.tsv.gz
 chmod 666 hits.tsv
 
 sudo -u postgres psql -t -c 'CREATE DATABASE test'
-sudo -u postgres psql test -t < create.sql
-time split hits.tsv --verbose  -n r/$(( $(nproc)/2 ))  --filter='sudo -u postgres psql test -t -c "\\copy hits FROM STDIN"'
+sudo -u postgres psql test -t <create.sql
+time split hits.tsv --verbose -n r/$(($(nproc) / 2)) --filter='sudo -u postgres psql test -t -c "\\copy hits FROM STDIN"'
 
 sudo -u postgres psql test -t -c 'CREATE EXTENSION pg_trgm;'
-time sudo -u postgres psql test -t < index.sql
+time sudo -u postgres psql test -t <index.sql
 
 time sudo -u postgres psql test -t -c 'VACUUM ANALYZE hits'
 
@@ -35,7 +41,7 @@ time sudo -u postgres psql test -t -c 'VACUUM ANALYZE hits'
 
 ./run.sh 2>&1 | tee log.txt
 
-sudo du -bcs /var/lib/postgresql/14/main/
+sudo du -bcs /var/lib/postgresql/$PGVERSION/main/
 
 cat log.txt | grep -oP 'Time: \d+\.\d+ ms' | sed -r -e 's/Time: ([0-9]+\.[0-9]+) ms/\1/' |
     awk '{ if (i % 3 == 0) { printf "[" }; printf $1 / 1000; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
--- postgresql/benchmark.sh
+++ postgresql/benchmark.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+PGVERSION=17
+
 sudo apt-get update
-sudo apt-get install -y postgresql-common
-sudo apt-get install -y postgresql-14
+sudo apt install -y postgresql-common
+sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y postgresql-$PGVERSION
 
 wget --no-verbose --continue 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
 gzip -d hits.tsv.gz
 chmod 777 ~ hits.tsv
 
 sudo -u postgres psql -t -c 'CREATE DATABASE test'
-sudo -u postgres psql test -t < create.sql
+sudo -u postgres psql test -t <create.sql
 sudo -u postgres psql test -t -c '\timing' -c "\\copy hits FROM 'hits.tsv' with freeze"
 
 sudo -u postgres psql test -t -c 'VACUUM ANALYZE hits'
@@ -19,7 +25,7 @@ sudo -u postgres psql test -t -c 'VACUUM ANALYZE hits'
 
 ./run.sh 2>&1 | tee log.txt
 
-sudo du -bcs /var/lib/postgresql/14/main/
+sudo du -bcs /var/lib/postgresql/$PGVERSION/main/
 
 cat log.txt | grep -oP 'Time: \d+\.\d+ ms' | sed -r -e 's/Time: ([0-9]+\.[0-9]+) ms/\1/' |
     awk '{ if (i % 3 == 0) { printf "[" }; printf $1 / 1000; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
