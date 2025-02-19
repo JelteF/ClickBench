bf9847f40031dfc78d2a237ccfe1a47aa5606a09
Jelte Fennema-Nio <github-tech@jeltef.nl>
2025-02-19 14:36:43 +0100

    Use postgres style load

--- pg_duckdb-postgres/benchmark.sh
+++ pg_duckdb-postgres/benchmark.sh
@@ -17,7 +17,10 @@ sudo docker run -d --name pgduck -p 5432:5432 -e POSTGRES_PASSWORD=duckdb -e PGD
 # Give postgres time to start running
 sleep 5
 
-./load.sh 2>&1 | tee load_log.txt
+psql <create.sql
+time split hits.tsv --verbose -n r/$(($(nproc) / 2)) --filter='psql -t -c "\\copy hits FROM STDIN"'
+
+time psql test -t -c 'VACUUM ANALYZE hits'
 
 ./run.sh 2>&1 | tee log.txt
 
