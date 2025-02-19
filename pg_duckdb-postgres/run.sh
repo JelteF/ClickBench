3bebc6c2600db49e30e99353f0509da57cdd6612
Jelte Fennema-Nio <github-tech@jeltef.nl>
2025-02-19 14:30:18 +0100

    Fix run script

--- pg_duckdb-postgres/run.sh
+++ pg_duckdb-postgres/run.sh
@@ -3,12 +3,9 @@
 TRIES=3
 CONNECTION=postgres://postgres:duckdb@localhost:5432/postgres
 
-DATABASE='ddb$pgclick'
-
 cat queries.sql | while read -r query; do
     echo "$query"
     (
-        echo "set search_path=$DATABASE;"
         echo '\timing'
         yes "$query" | head -n $TRIES
     ) | psql -X -t $CONNECTION | grep 'Time'
