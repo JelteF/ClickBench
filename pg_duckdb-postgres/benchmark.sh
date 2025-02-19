3d8a355ab7da07605b1930096615ce4050524aef
Jelte Fennema-Nio <github-tech@jeltef.nl>
2025-02-19 13:06:08 +0100

    Add pg_duckdb-postgres directory

---
+++ pg_duckdb-postgres/benchmark.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+# Setup on Ubuntu (your package manager may vary):
+# sudo snap install docker
+# sudo apt install postgresql-client
+
+wget --no-verbose --continue https://datasets.clickhouse.com/hits_compatible/athena/hits.parquet
+sudo docker run -d --name pgduck -p 5432:5432 -e POSTGRES_PASSWORD=duckdb -v ./hits.parquet:/tmp/hits.parquet pgduckdb/pgduckdb:17-v0.3.1 -c duckdb.max_memory=10GB
+
+# Give postgres time to start running
+sleep 5
+
+./load.sh 2>&1 | tee load_log.txt
+
+./run.sh 2>&1 | tee log.txt
+
+# Go to https://app.motherduck.com and execute:
+# `SELECT database_size FROM pragma_database_size() WHERE database_name = 'pgclick'`
+# 25 GB
+
+cat log.txt | grep -oP 'Time: \d+\.\d+ ms' | sed -r -e 's/Time: ([0-9]+\.[0-9]+) ms/\1/' |
+    awk '{ if (i % 3 == 0) { printf "[" }; printf $1 / 1000; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
---
+++ pg_duckdb-postgres/load.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+CONNECTION=postgres://postgres:duckdb@localhost:5432/postgres
+PSQL=psql
+
+DATABASE='ddb$pgclick'
+PARQUET_FILE='https://datasets.clickhouse.com/hits_compatible/hits.parquet'
+
+echo "Loading data"
+(
+    echo "\timing"
+    cat create.sql |
+        sed -e "s=REPLACE_SCHEMA=$DATABASE=g" -e "s=REPLACE_PARQUET_FILE=$PARQUET_FILE=g"
+) | $PSQL $CONNECTION | grep 'Time'
