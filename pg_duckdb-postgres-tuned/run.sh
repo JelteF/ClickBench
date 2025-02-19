c26fb198a73918a2839d510bf6fadff2af613a15
Jelte Fennema-Nio <github-tech@jeltef.nl>
2025-02-19 16:54:58 +0100

    Force duckdb execution

--- pg_duckdb-postgres-tuned/run.sh
+++ pg_duckdb-postgres-tuned/run.sh
@@ -6,6 +6,7 @@ CONNECTION=postgres://postgres:duckdb@localhost:5432/postgres
 cat queries.sql | while read -r query; do
     echo "$query"
     (
+        echo 'set duckdb.force_execution = true;'
         echo '\timing'
         yes "$query" | head -n $TRIES
     ) | psql -X -t $CONNECTION | grep 'Time'
